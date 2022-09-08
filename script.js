fa8a005448428ece71bef941529c1175a9b8e246
DJV <djtvaq@gmail.com>
2022-09-07 22:39:44 -0400

    Add event listeners to every button and update display when a button is pressed

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ function operate(operator, a, b){
 return operator(a,b)
 
 }
+
+const calcButtons = document.querySelectorAll('.buttonGeneric')
+const displayScreen = document.querySelector('#displayScreen')
+
+function updateDisplay() {
+   displayScreen.textContent = this.textContent 
+
+}
+calcButtons.forEach(button => button.addEventListener('click', updateDisplay))
