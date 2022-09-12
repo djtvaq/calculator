db268b0df5811b02c75a81663f766502efe85349
DJV <djtvaq@gmail.com>
2022-09-12 19:53:11 -0400

    Remove previous code, write out pseudocode

--- script.js
+++ script.js
@@ -1,100 +1,13 @@
-const displayScreen = document.querySelector('#displayScreen')
-const buttonDigits = document.querySelectorAll('.buttonDigit')
-const buttonAdd = document.querySelector('#add')
-const buttonEqual = document.querySelector('#equal')
-
-
-
-let inputValueArray = []
-let totalValueArray =[]
-let valueA = 0
-let valueB = 0
-let operator;
-
-buttonDigits.forEach(button => button.addEventListener('click', collectInput))
-buttonAdd.addEventListener('click', operate)
-buttonEqual.addEventListener('click', collectValueB)
-
-
-
-function collectInput() {
-    inputValueArray.push(this.textContent);
-    updateDisplay()
-}
-
-function updateDisplay() {
-    let inputValueString = inputValueArray.join('')
-    let inputValueInt = parseInt(inputValueString)
-    displayScreen.textContent = inputValueInt
-    valueA = inputValueInt
-}
-
-
-
-function operate() {
-    operator = this.id
-
-    if (operator === 'add') {
-        displayScreen.textContent = add(valueA, valueB)
-        valueA = parseInt(displayScreen.textContent)
-        resetDisplay()
-    } else {
-        console.log('oop')
-    }
-    }
-
-
-
-function resetDisplay() {
-    // When an operator is pressed
-    //display the static current value
-    //but quietly reset the value
-    displayScreen.textContent = valueA
-    inputValueArray = []
-}
-
-function add(a, b) {
-    return a + b
-}
-
-function subtract(a, b) {
-    return a - b
-}
-
-function divide(a, b) {
-    return a / b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-
-function collectValueB() {
-    let inputValueString = inputValueArray.join('')
-    let inputValueInt = parseInt(inputValueString)
-    valueB = inputValueInt
-    operate()
-}
-
-function collectValue() {
-    let inputValueString = inputValueArray.join('')
-    let inputValueInt = parseInt(inputValueString)
-    valueA = inputValueInt
-    resetDisplay()
-}
+//take user prompt number - store in variable1
+//take user operator
+//take user prompt number - store in variable 2
+//evaluate the equation - store answer in variable 1 
+//take user operator
+// if eqal is chosen, display variable 1
+//if a new operator is chosen
+//take user promp for number - store in variable 2
+//evaluate the equation
 
 
 
 
-
-
-
-
-
-
-//When a user hits the number key, the numbers should concat and appear on the screen.
-//When a user hits the decimal key, a period should be concat in the numbers
-//When a user hits the operator key, the current value on the screen should be held as n1.
-//when the user hits the equals kee
-// we need to decide what to do depending on what button is pressed
