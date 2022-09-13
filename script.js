43e2207087fa53104607642b23345219dea911df
DJV <djtvaq@gmail.com>
2022-09-12 20:38:15 -0400

    Create DOM Element variables for each calculator button and add click event listeners, Create function to accept and display user input, Create funtion to accept and store user input operation

--- script.js
+++ script.js
@@ -9,18 +9,45 @@
 //evaluate the equation
 
 
+const displayScreen = document.querySelector('#displayScreen')
+const buttonDigits = document.querySelectorAll('.buttonDigit')
+const buttonOperators = document.querySelectorAll('.buttonOperation')
+
+buttonDigits.forEach(button => button.addEventListener('click', userInputNumber))
+buttonOperators.forEach(button => button.addEventListener('click', userInputOperator))
 
 
 
 let valueA = 0
-let operator 
+let valueB = 0
+let operator
+let userInputArray = []
+let userInputString
+let userInputInteger
 
 
+
+function userInputNumber() {
+    let userInput = parseInt(this.textContent)
+    userInputArray.push(userInput)
+    userInputString = userInputArray.join('')
+    userInputInteger = parseInt(userInputString)
+    displayScreen.textContent = userInputInteger
+    valueA = userInputInteger
+}
 
 function userPromptNumber() {
     let userPrompt = prompt('What is your first number')
     valueA = parseInt(userPrompt)
     userPromptOperator()
+}
+
+
+
+function userInputOperator() {
+    operator = this.id
+    displayScreen.textContent = valueA
+    evaluation()
 }
 
 function userPromptOperator() {
@@ -29,37 +56,48 @@ function userPromptOperator() {
     evaluate()
 }
 
+
+
+function evaluation() {
+    if (operator === '+') {
+        
+
+    }
+
+
+
+}
+
 function evaluate() {
 
-if (operator === '+') {
-    let valueB = parseInt(prompt('What is your next number?'))
-    let answer = valueA+valueB
-    console.log(answer)
-    valueA = answer
-    userPromptOperator()
-} else if (operator === '-') {
-    let valueB = parseInt(prompt('What is your next number?'))
-    let answer = valueA-valueB
-    console.log(answer)
-    valueA = answer
-    userPromptOperator()
-} else if (operator === '*') {
-    let valueB = parseInt(prompt('What is your next number?'))
-    let answer = valueA*valueB
-    console.log(answer)
-    valueA = answer
-    userPromptOperator()
-} else if (operator === '/') {
-    let valueB = parseInt(prompt('What is your next number?'))
-    let answer = valueA/valueB
-    console.log(answer)
-    valueA = answer
-    userPromptOperator()
-} else if (operator === '=') {
-    let answer = valueA
-    console.log(answer)
-}
+    if (operator === '+') {
+        let valueB = parseInt(prompt('What is your next number?'))
+        let answer = valueA + valueB
+        console.log(answer)
+        valueA = answer
+        userPromptOperator()
+    } else if (operator === '-') {
+        let valueB = parseInt(prompt('What is your next number?'))
+        let answer = valueA - valueB
+        console.log(answer)
+        valueA = answer
+        userPromptOperator()
+    } else if (operator === '*') {
+        let valueB = parseInt(prompt('What is your next number?'))
+        let answer = valueA * valueB
+        console.log(answer)
+        valueA = answer
+        userPromptOperator()
+    } else if (operator === '/') {
+        let valueB = parseInt(prompt('What is your next number?'))
+        let answer = valueA / valueB
+        console.log(answer)
+        valueA = answer
+        userPromptOperator()
+    } else if (operator === '=') {
+        let answer = valueA
+        console.log(answer)
+    }
 }
 
 
-userPromptNumber()
